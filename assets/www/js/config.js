dbc7d8ba08fa94a537620cab29dcf9ca8b2ad67e
Leandro <learod17@gmail.com>
2013-03-11 12:25:10 -0300

    release-02

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -1,8 +1,8 @@
-// var host = "sondeociudadania.com.ar:3000";
+var host = "sondeociudadania.com.ar";
 
 // var host = "192.168.1.137:3000";
 // var host = "192.168.1.94:3000";
-var host = "192.168.43.153:3000";
+// var host = "192.168.43.153:3000";
 
 // var puerto = "3000";
 
